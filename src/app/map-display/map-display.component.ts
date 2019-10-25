28d142a4526957138e5aa3a4ba05789dde29724b
randerson9 <richardanderson9225@gmail.com>
2019-10-25 18:26:43 -0400

    initial commit
    just the basics!

---
+++ src/app/map-display/map-display.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-display',
+  templateUrl: './map-display.component.html',
+  styleUrls: ['./map-display.component.css']
+})
+
+export class MapDisplayComponent {}
